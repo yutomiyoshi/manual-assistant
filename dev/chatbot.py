e49c4902164ee5d8e500ccfc423c8646a3599786
yutomiyoshi <seanayuuto0826@gmail.com*>
2025-01-19 10:11:43 +0000

    updata dev for adding-chatbot-base

---
+++ dev/chatbot.py
@@ -0,0 +1,23 @@
+"""
+チャットボット
+
+"""
+class Chatbot():
+
+    """
+    """
+    def __init__(
+        self,
+        llm_name: str = "", ## LLMモデルの名前（huggingfaceリポジトリに準拠）
+    ):
+        pass
+    
+    """
+    チャットボットが考えた文章を返す
+    """
+    def generate_sentence(
+        self,
+        input_sentence: str ## 入力する文章、クエリー
+    ):
+        pass
+        
